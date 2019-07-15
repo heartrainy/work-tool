bfbbd9132f94bb5202d1270e747f4d8779233723
heartrainy <526132448@qq.com>
2019-07-15 14:17:18 +0800

    压缩dist文件

--- src/compress.js
+++ src/compress.js
@@ -10,7 +10,7 @@ function compressDir () {
   // 获取项目文件名
   const projectName = path.basename(process.cwd());
   // 压缩文件
-  compressing.zip.compressDir(path.resolve('.', './test'), path.resolve('.', `./${projectName}.zip`))
+  compressing.zip.compressDir(path.resolve('.', './dist'), path.resolve('.', `./${projectName}.zip`))
   .then(() => {
       console.log('压缩成功');
   })
